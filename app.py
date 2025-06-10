fd2cb682d707481cdd0cfb2548a89f322523f679
imran-test <imranmohammad8531@gmail.com>
2025-06-10 10:21:50 +0530

    testing pipeline by updating code

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ import json
 def lambda_handler(event, context):
     return {
         "statusCode": 200,
-        "body": json.dumps("Hello from CI/CD Lambda!")
+        "body": json.dumps("Hello from CI/CD Lambda  --- updatation for testing!")
     }
